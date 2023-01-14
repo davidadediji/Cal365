392770fd144e2ad88795e6ce1c0b835496a0701c
davidadediji <davidadediji@gmail.com>
2023-01-14 20:06:56 +0100

    chore:

---
+++ src/main/java/com/cal365/Cal365Application.java
@@ -0,0 +1,13 @@
+package com.cal365;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cal365Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Cal365Application.class, args);
+    }
+}
+
+
+
--- src/test/java/com/cal365/AppApplicationTests.java
+++ src/test/java/com/cal365/AppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cal365.app;
+package com.cal365;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
