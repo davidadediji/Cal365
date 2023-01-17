97334ec81c4924d3def86a32413ded2167dd2b9f
davidadediji <davidadediji@gmail.com>
2023-01-17 14:48:43 +0100

    chore: remove client folder

--- src/main/java/com/cal365/Cal365Application.java
+++ src/main/java/com/cal365/Cal365Application.java
@@ -3,6 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -10,12 +11,6 @@ public class Cal365Application {
     public static void main(String[] args) {
         SpringApplication.run(Cal365Application.class, args);
     }
-    @GetMapping("/get")
-    public GreetResponse greet(){
-        return new GreetResponse("Hello", 24);
-    }
-
-    public record GreetResponse(String greet, int age){}
 }
 
 
